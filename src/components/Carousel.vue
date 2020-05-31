9f0ef47deb2a240c7c648fc8fcc01984d923300c
Tom Sisman <tomsisman@toms-mbp.home>
2020-05-31 13:35:13 +0100

    add interactivity to slider

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -1,10 +1,13 @@
 <template>
   <div class="carousel-container">
-    <div class="carousel">
-      <CarouselItem v-for="item in data" :key="item.code" :item="item" />
+    <div class="carousel-track">
+      <div class="carousel">
+        <CarouselItem v-for="item in data" :key="item.code" :item="item" :classList="carouselItemclassList" />
+      </div>
     </div>
-    <button class="carousel-button carousel-button--prev"><i class="fas fa-chevron-left"></i></button>
-    <button class="carousel-button carousel-button--next"><i class="fas fa-chevron-right"></i></button>
+
+    <button v-on:click="handlePrev" class="carousel-button carousel-button--prev"><i class="fas fa-chevron-left"></i></button>
+    <button v-on:click="handleNext" class="carousel-button carousel-button--next"><i class="fas fa-chevron-right"></i></button>
   </div>
 </template>
 
@@ -20,6 +23,43 @@ export default {
     data: {
       type: Array
     }
+  },
+  methods: {
+    handlePrev: function() {
+      if (this.canSliderUpdate) {
+        this.canSliderUpdate = false;
+        this.carouselItemclassList = 'carousel-item__transition-prev'
+        setTimeout(() => {
+          this.data.push(this.data[0])
+          this.data.shift();
+          this.carouselItemclassList = ''
+          this.canSliderUpdate = true;
+        }, 500)
+      }
+    },
+    handleNext: function() {
+      if (this.canSliderUpdate) {
+        this.canSliderUpdate = false;
+        this.data.unshift(this.data[this.data.length - 1])
+        this.data.pop();
+        this.carouselItemclassList = 'carousel-item__transition-next'
+        setTimeout(() => {
+          this.carouselItemclassList = ''
+          this.canSliderUpdate = true;
+        }, 500);
+      }
+    }
+  }, 
+  data: function() {
+    return {
+      carouselItemclassList: '',
+      canSliderUpdate: true
+    }
+  },
+  beforeMount() {
+    window.setInterval(() => {
+      this.handleNext();
+    }, 5000);   
   }
 }
 </script>
--- src/components/carousel/CarouselItem.vue
+++ src/components/carousel/CarouselItem.vue
@@ -1,11 +1,13 @@
 <template>
-  <div class="carousel-item">
+  <div class="carousel-item carousel-item__transition-auto" :class="classList">
     <a class="carousel-item__container" :href="item.url">
       <div class="carousel-item__image-container">
         <img class="carousel-item__image" :title="item.name" :alt="item.productImageAltText" :src="item.productImageUrl">
       </div>
       <div>
-        <h3 class="carousel-item__title">{{item.name}}</h3>
+        <div class="carousel-item__header-container">
+          <h3 class="carousel-item__title">{{item.name}}</h3>
+        </div>
         <!-- I hardcoded the below as stock levels didn't appear to be in the JSON -->
         <p class="carousel-item__stock">Stock: <span>10</span></p>
         <p class="carousel-item__price">{{item.price.currency}} <span>{{item.price.formattedValue}}</span></p>
@@ -21,7 +23,10 @@ export default {
   props: {
     item: {
       type: Object
-    }
+    },
+    classList: {
+      type: String
+    },
   }
 }
 </script>
